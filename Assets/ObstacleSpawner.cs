0ca397887d3a04dac46470d8f96558fbbd1e4f16
ismail <ismailtriki422@gmail.com>
2018-07-18 17:34:45 +0100

    obstacles

---
+++ Assets/ObstacleSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+
+    public float minY;
+    public float maxY;
+    public float distance;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Obstacle")
+        {
+            
+            float obstacleY = Random.Range(minY, maxY);
+            
+            Vector3 spawnPosition = new Vector3(transform.position.x + distance, obstacleY, 0);
+          
+            col.gameObject.transform.position = spawnPosition;
+
+        }
+    }
+}
